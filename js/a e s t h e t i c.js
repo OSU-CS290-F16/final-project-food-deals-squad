580cfbdbb2fbe8a39d627bc4b8ca8b1b8245de8e
vpiscitello <piscitev@oregonstate.edu>
2016-11-22 20:37:29 -0800

    Completed form setup and some animations
    -Custom Form Checkbox
    -Custom Checkbox Animations
    -Checkbox tooltip
    -Slideout Modal and Overlay Animation

--- js/a e s t h e t i c.js
+++ js/a e s t h e t i c.js
@@ -2,22 +2,43 @@ $(document).click(function(e) {
     var html = $("html"), body = $("body"), overlay = $(".overlay");
     var addDealButton = $("#add-deal-button"), addDealModal = $(".add-deal-modal");
 
-    if(overlay.is(e.target)) {
-        html.css("overflow-y", "");
-        body.css("position", "").css("right", "");
-        addDealModal.addClass("hidden"); overlay.addClass("hidden");
-    }
+    $(function checkboxAction() {
+        var checkbox =  $("#free-checkbox"), checked = document.getElementById("free-checkbox").checked;
+        var clickableCheckbox = $(".clickable-checkbox"), visualCheckbox = $(".visual-checkbox");
+        var checkmarkPlace = $("p", visualCheckbox);
+        var checkmark = "&#x2714;";
+
+        if(checkbox.is(e.target) && checked) {
+            clickableCheckbox.removeClass("unchecked"); clickableCheckbox.addClass("checked");
+            checkmarkPlace.html(checkmark);
+        } else if(checkbox.is(e.target) && !checked) {
+            clickableCheckbox.removeClass("checked"); clickableCheckbox.addClass("unchecked");
+            checkmarkPlace.html("");
+        }
+    });
+
+    $(function modalAction() {
+        if(overlay.is(e.target)) {
+            html.css("overflow-y", ""); body.removeClass("open");
+            overlay.removeClass("visible");
+            setTimeout(function() { addDealModal.removeClass("visible"); }, 1000);
+        }
+    });
+
 
 });
 
-$(function addDeal(){
+$(function addDeal() {
     var html = $("html"), body = $("body"), overlay = $(".overlay");
     var addDealButton = $("#add-deal-button"), addDealModal = $(".add-deal-modal");
 
     $(document).on("click", "#add-deal-button", function() {
-        html.css("overflow-y", "hidden");
-        body.css("position", "relative").css("right", "600px");
-        addDealModal.removeClass("hidden"); overlay.removeClass("hidden");
+        html.css("overflow-y", "hidden"); body.addClass("open");
+        addDealModal.addClass("visible"); overlay.addClass("visible");
     });
 
 });
+
+function clearForm() {
+
+}
